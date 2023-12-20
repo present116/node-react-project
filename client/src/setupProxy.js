6d6817e95668a63207c648d47a19d9de033199b3
고현재 <hyunjae@gohyeonjaes-MacBook-Air.local>
2023-12-20 14:37:38 +0900

    concurrently

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,10 +1,10 @@
-const { proxy } = require('http-proxy-middleware')
-module.exports = function(app) {
-    app.use(
-        '/api',
-        proxy({
-            target: 'http://localhost:8081',
-            changeOrigin: true,
-        })
-    )
-}
+const { createProxyMiddleware } = require("http-proxy-middleware");
+module.exports = function (app) {
+  app.use(
+    "/api",
+    createProxyMiddleware({
+      target: "http://localhost:8081",
+      changeOrigin: true,
+    })
+  );
+};
