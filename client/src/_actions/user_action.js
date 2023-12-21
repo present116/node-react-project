97df39fb67caeba05c4ad8a594a3701440f1f118
고현재 <hyunjae@gohyeonjaes-MacBook-Air.local>
2023-12-21 12:20:10 +0900

    register react

--- client/src/_actions/user_action.js
+++ client/src/_actions/user_action.js
@@ -1,16 +1,26 @@
 import axios from "axios";
-import { LOGIN_USER } from './types'
+import { LOGIN_USER, REGISTER_USER } from './types'
 
 export function loginUser(dataToSubmit) {
-    console.log('dataToSubmit', dataToSubmit)
 
     const requeset = axios.post('/api/users/login', dataToSubmit)
         .then(res => res.data)
-    console.log('request', requeset)
 
     // Reducer로 보낸다
     return {
         type: LOGIN_USER,
+        payload: requeset
+    }
+}
+
+export function registerUser(dataToSubmit) {
+
+    const requeset = axios.post('/api/users/register', dataToSubmit)
+        .then(res => res.data)
+
+    // Reducer로 보낸다
+    return {
+        type: REGISTER_USER,
         payload: requeset
     }
 }
--- client/src/components/views/LoginPage/LoginPage.js
+++ client/src/components/views/LoginPage/LoginPage.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react'
-import { Axios } from 'axios';
 import { useDispatch} from 'react-redux'
 import { loginUser } from '../../../_actions/user_action';
 
@@ -28,6 +27,7 @@ function LoginPage() {
     dispatch(loginUser(body))
       .then(res => {
         if(res.payload.loginSuccess){
+          alert("성공")
           //props.history.push('/')
         }
       })
